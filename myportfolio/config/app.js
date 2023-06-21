468ee4664b3ef85e229fa1db25f77a05777b611a
nampavel <115383491+nampavel@users.noreply.github.com>
2023-06-21 03:05:35 -0400

    Created and updated pages
    Add information to the pages

--- myportfolio/config/app.js
+++ myportfolio/config/app.js
@@ -4,23 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +44,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
